2ed17c1855cd9ddab92fca97b12092272020f2b2
Esteban Aranguri <estebanaranguri11@gmail.com>
2024-05-25 07:53:00 -0300

    ajustando medidas generales

---
+++ main.js
@@ -0,0 +1,86 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.menu-btn').classList.toggle('show');
+    document.querySelector('.showcase').classList.toggle('show');
+    document.querySelector('.nav-menu').classList.toggle('show');
+    document.querySelector('.nav-main').classList.toggle('show');
+    document.querySelector('.xmark').classList.toggle('show');
+    document.querySelector('.footer').classList.toggle('show');
+    document.querySelector('.social').classList.toggle('show');
+})
+
+document.querySelector('.xmark').addEventListener('click', () => {
+    document.querySelector('.xmark').classList.toggle('show');
+    document.querySelector('.menu-btn').classList.toggle('show');
+    document.querySelector('.nav-menu').classList.toggle('show');
+    document.querySelector('.nav-main').classList.toggle('show');
+    document.querySelector('.showcase').classList.toggle('show');
+    document.querySelector('.footer').classList.toggle('show');
+    document.querySelector('.social').classList.toggle('show');
+})
+
+document.querySelector('.home').addEventListener('click', () => {
+    document.querySelector('.xmark').classList.toggle('show');
+    document.querySelector('.menu-btn').classList.toggle('show');
+    document.querySelector('.nav-menu').classList.toggle('show');
+    document.querySelector('.nav-main').classList.toggle('show');
+    document.querySelector('.showcase').classList.toggle('show');
+    document.querySelector('.footer').classList.toggle('show');
+    document.querySelector('.social').classList.toggle('show');
+})
+
+
+document.querySelector('.about').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+    document.querySelector('.xmark').classList.toggle('show');
+    document.querySelector('.about-me').classList.toggle('show');
+    document.querySelector('.back-ab').classList.toggle('show');
+    document.querySelector('.footer').classList.toggle('show');
+    document.querySelector('.social').classList.toggle('show');
+})
+
+
+document.querySelector('.back-ab').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+    document.querySelector('.about-me').classList.toggle('show');
+    document.querySelector('.xmark').classList.toggle('show');
+    document.querySelector('.back-ab').classList.toggle('show');
+    document.querySelector('.footer').classList.toggle('show');
+    document.querySelector('.social').classList.toggle('show');
+})
+
+document.querySelector('.projects').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+    document.querySelector('.xmark').classList.toggle('show');
+    document.querySelector('.pro-main').classList.toggle('show');
+    document.querySelector('.back-pro').classList.toggle('show');
+    document.querySelector('.footer').classList.toggle('show');
+    document.querySelector('.social').classList.toggle('show');
+})
+
+document.querySelector('.back-pro').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+    document.querySelector('.xmark').classList.toggle('show');
+    document.querySelector('.pro-main').classList.toggle('show');
+    document.querySelector('.back-pro').classList.toggle('show');
+    document.querySelector('.projects').classList.toggle('show');
+    document.querySelector('.footer').classList.toggle('show');
+    document.querySelector('.social').classList.toggle('show');
+})
+
+document.querySelector('.contact').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+    document.querySelector('.xmark').classList.toggle('show');
+    document.querySelector('.email').classList.toggle('show');
+    document.querySelector('.back-email').classList.toggle('show');
+    document.querySelector('.footer').classList.toggle('show');
+    document.querySelector('.social').classList.toggle('show');
+})
+
+document.querySelector('.back-email').addEventListener('click', () => {
+    document.querySelector('.back-email').classList.toggle('show');
+    document.querySelector('.nav-menu').classList.toggle('show');
+    document.querySelector('.xmark').classList.toggle('show');
+    document.querySelector('.email').classList.toggle('show');
+    document.querySelector('.footer').classList.toggle('show');
+    document.querySelector('.social').classList.toggle('show');
+})
